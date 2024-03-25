4eaae638bee5eaf3f525d4b75735f8b7aa8f6429
jonatasjdb <jonatasjdb@hotmail.com>
2024-03-25 11:21:27 -0300

    add search gyms nearby

---
+++ src/use-case/fetch-nearby-gyms.ts
@@ -0,0 +1,29 @@
+import { GymsRepository } from '@/repositories/gyms-respository'
+import { Gym } from '@prisma/client'
+
+interface FetchNearbyGymsUseCaseRequest {
+  userLatitude: string
+  userLongitude: string
+}
+
+interface FetchNearbyGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class FetchNearbyGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    userLatitude,
+    userLongitude,
+  }: FetchNearbyGymsUseCaseRequest): Promise<FetchNearbyGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.findManyNearby({
+      latitude: userLatitude,
+      longitude: userLongitude,
+    })
+
+    return {
+      gyms,
+    }
+  }
+}
